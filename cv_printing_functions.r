d87b408ace5d175878d7f4e1799dd6ee7bee7811
jodavid <jodavid@protonmail.com>
2022-06-15 21:12:27 -0300

    update CV

--- cv_printing_functions.r
+++ cv_printing_functions.r
@@ -22,31 +22,31 @@ create_CV_object <-  function(data_location,
                               pdf_mode = FALSE,
                               sheet_is_publicly_readable = TRUE,
                               cache_data = TRUE) {
-  
+
   cv <- list(
     pdf_mode = pdf_mode,
     links = c(),
     cache_data = cache_data
   ) %>%
     load_data(data_location, sheet_is_publicly_readable)
-  
-  
+
+
   extract_year <- function(dates){
     date_year <- stringr::str_extract(dates, "(20|19)[0-9]{2}")
     date_year[is.na(date_year)] <- lubridate::year(lubridate::ymd(Sys.Date())) + 10
-    
+
     date_year
   }
-  
+
   parse_dates <- function(dates){
-    
+
     date_month <- stringr::str_extract(dates, "(\\w+|\\d+)(?=(\\s|\\/|-)(20|19)[0-9]{2})")
     date_month[is.na(date_month)] <- "1"
-    
+
     paste("1", date_month, extract_year(dates), sep = "-") %>%
       lubridate::dmy()
   }
-  
+
   # Clean up entries dataframe to format we need it for printing
   cv$entries_data %<>%
     tidyr::unite(
@@ -74,7 +74,7 @@ create_CV_object <-  function(data_location,
     ) %>%
     dplyr::arrange(desc(parse_dates(end))) %>%
     dplyr::mutate_all(~ ifelse(is.na(.), 'N/A', .))
-  
+
   cv
 }
 
@@ -83,22 +83,22 @@ load_data <- function(cv, data_location, sheet_is_publicly_readable){
   cache_loc <- "ddcv_cache.rds"
   has_cached_data <- fs::file_exists(cache_loc)
   is_google_sheets_location <- stringr::str_detect(data_location, "docs\\.google\\.com")
-  
+
   if(has_cached_data & cv$cache_data){
-    
+
     cv <- c(cv, readr::read_rds(cache_loc))
   } else if(is_google_sheets_location){
     if(sheet_is_publicly_readable){
       # This tells google sheets to not try and authenticate. Note that this will only
       # work if your sheet has sharing set to "anyone with link can view"
-      googlesheets4::sheets_deauth()
+      googlesheets4::gs4_deauth()  #sheets_deauth()
     } else {
       # My info is in a public sheet so there's no need to do authentication but if you want
       # to use a private sheet, then this is the way you need to do it.
       # designate project-specific cache so we can render Rmd without problems
       options(gargle_oauth_cache = ".secrets")
     }
-    
+
     read_gsheet <- function(sheet_id){
       googlesheets4::read_sheet(data_location, sheet = sheet_id, skip = 1, col_types = "c")
     }
@@ -113,7 +113,7 @@ load_data <- function(cv, data_location, sheet_is_publicly_readable){
     cv$text_blocks  <- readr::read_csv(paste0(data_location, "text_blocks.csv"), skip = 1)
     cv$contact_info <- readr::read_csv(paste0(data_location, "contact_info.csv"), skip = 1)
   }
-  
+
   if(cv$cache_data & !has_cached_data){
     # Make sure we only cache the data and not settings etc.
     readr::write_rds(
@@ -125,10 +125,10 @@ load_data <- function(cv, data_location, sheet_is_publicly_readable){
       ),
       cache_loc
     )
-    
+
     cat(glue::glue("CV data is cached at {cache_loc}.\n"))
   }
-  
+
   invisible(cv)
 }
 
@@ -138,27 +138,27 @@ sanitize_links <- function(cv, text){
   if(cv$pdf_mode){
     link_titles <- stringr::str_extract_all(text, '(?<=\\[).+?(?=\\]\\()')[[1]]
     link_destinations <- stringr::str_extract_all(text, '(?<=\\]\\().+?(?=\\))')[[1]]
-    
+
     n_links <- length(cv$links)
     n_new_links <- length(link_titles)
-    
+
     if(n_new_links > 0){
       # add links to links array
       cv$links <- c(cv$links, link_destinations)
-      
+
       # Build map of link destination to superscript
       link_superscript_mappings <- purrr::set_names(
         paste0("<sup>", (1:n_new_links) + n_links, "</sup>"),
         paste0("(", link_destinations, ")")
       )
-      
+
       # Replace the link destination and remove square brackets for title
       text <- text %>%
         stringr::str_replace_all(stringr::fixed(link_superscript_mappings)) %>%
         stringr::str_replace_all('\\[(.+?)\\](?=<sup>)', "\\1")
     }
   }
-  
+
   list(cv = cv, text = text)
 }
 
@@ -166,7 +166,7 @@ sanitize_links <- function(cv, text){
 #' @description Take a position data frame and the section id desired and prints the section to markdown.
 #' @param section_id ID of the entries section to be printed as encoded by the `section` column of the `entries` table
 print_section <- function(cv, section_id, glue_template = "default"){
-  
+
   if(glue_template == "default"){
     glue_template <- "
 ### {title}
@@ -180,9 +180,9 @@ print_section <- function(cv, section_id, glue_template = "default"){
 {description_bullets}
 \n\n\n"
   }
-  
+
   section_data <- dplyr::filter(cv$entries_data, section == section_id)
-  
+
   # Take entire entries data frame and removes the links in descending order
   # so links for the same position are right next to each other in number.
   for(i in 1:nrow(section_data)){
@@ -192,9 +192,9 @@ print_section <- function(cv, section_id, glue_template = "default"){
       cv <- strip_res$cv
     }
   }
-  
+
   print(glue::glue_data(section_data, glue_template))
-  
+
   invisible(strip_res$cv)
 }
 
@@ -205,11 +205,11 @@ print_section <- function(cv, section_id, glue_template = "default"){
 print_text_block <- function(cv, label){
   text_block <- dplyr::filter(cv$text_blocks, loc == label) %>%
     dplyr::pull(text)
-  
+
   strip_res <- sanitize_links(cv, text_block)
-  
+
   cat(strip_res$text)
-  
+
   invisible(strip_res$cv)
 }
 
@@ -218,7 +218,7 @@ print_text_block <- function(cv, label){
 #' @description Construct a bar chart of skills
 #' @param out_of The relative maximum for skills. Used to set what a fully filled in skill bar is.
 print_skill_bars <- function(cv, out_of = 5, bar_color = "#969696", bar_background = "#d9d9d9", glue_template = "default"){
-  
+
   if(glue_template == "default"){
     glue_template <- "
 <div
@@ -232,7 +232,7 @@ print_skill_bars <- function(cv, out_of = 5, bar_color = "#969696", bar_backgrou
     dplyr::mutate(width_percent = round(100*as.numeric(level)/out_of)) %>%
     glue::glue_data(glue_template) %>%
     print()
-  
+
   invisible(cv)
 }
 
@@ -250,12 +250,12 @@ Links {data-icon=link}
 
 
 ")
-    
+
     purrr::walk2(cv$links, 1:n_links, function(link, index) {
       print(glue::glue('{index}. {link}'))
     })
   }
-  
+
   invisible(cv)
 }
 
@@ -267,6 +267,6 @@ print_contact_info <- function(cv){
     cv$contact_info,
     "- <i class='fa fa-{icon}'></i> {contact}"
   ) %>% print()
-  
+
   invisible(cv)
 }
